7f764d910702412c5d0ad13caa00c093af4f5531
cesspionline-web <samucp.online@gmail.com>
2025-09-16 20:07:41 +0200

    Delete js directory

--- js/Js2.js
+++
@@ -1,8 +0,0 @@
-function sendMessage() {
-  const phoneNumber = '+265885883937'; // Replace with your WhatsApp number
-  const name = document.getElementById('name').value;
-  const message = document.getElementById('message').value;
-  const whatsappMessage = `Hello, my name is ${name}. ${message}`;
-  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
-  window.open(url, '_blank');
-}
